1ff30b14dba0dbc45dba60d695d82dc68e449d44
Dan Zimmerman <code@danzimmerman.com>
2026-02-08 17:09:56 -0500

    Prints correct value but crashes the callback (seems to return anyway?)

--- bare_zenoh_interop_pixi/standalone_zenoh_listener.py
+++ bare_zenoh_interop_pixi/standalone_zenoh_listener.py
@@ -1,16 +1,21 @@
 # Kind of following https://zenoh.io/blog/2021-04-28-ros2-integration/
 # But trying rosbags.serde instead of yanked pycdr
 import zenoh
-import rosbags
+from rosbags.typesys import Stores, get_typestore
 
-config = zenoh.Config.from_file("config.json5")
-print("Opening zenoh session")
-
+jazzy_typestore = get_typestore(Stores.ROS2_JAZZY)
 
 def sample_callback(sample):
     # msg_str = stdMsgString.deserialize(sample.payload)
-    rosbags.serde.deserialize(sample.payload, "std_msgs/msg/String")
-    print(f"{sample.key_expr} => {sample.payload.to_string()}")
+    received_msg = jazzy_typestore.deserialize_cdr(
+        sample.payload.to_bytes(), "std_msgs/msg/String"
+    )
+    print("Type of received_msg:", type(received_msg))
+    print(f"{sample.key_expr} => {str(received_msg.data)}")
+
+
+config = zenoh.Config.from_file("config.json5")
+print("Opening zenoh session")
 
 session = zenoh.open(config)
 session_info = session.info
