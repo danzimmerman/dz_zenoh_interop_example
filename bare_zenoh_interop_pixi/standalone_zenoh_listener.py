c5bf680f5454276cc848034d7cc5195367b92ce2
Dan Zimmerman <code@danzimmerman.com>
2026-02-08 16:18:44 -0500

    Try client mode and callback

--- bare_zenoh_interop_pixi/standalone_zenoh_listener.py
+++ bare_zenoh_interop_pixi/standalone_zenoh_listener.py
@@ -1,13 +1,17 @@
 # Kind of following https://zenoh.io/blog/2021-04-28-ros2-integration/
 # But trying rosbags.serde instead of yanked pycdr
 import zenoh
-import rosbags
-from cdr_decoder import CDRStructBase
 
 config = zenoh.Config.from_file("config.json5")
 print("Opening zenoh session")
-with zenoh.open(config) as session:
-    print("Declaring subscriber for chatter")
-    with session.declare_subscriber("chatter/**") as subscriber:
-        for sample in subscriber:
-            print(f"{sample.key_expr} => {sample.payload.to_string()}")
+
+
+def sample_callback(sample):
+    print(f"{sample.key_expr} => {sample.payload.to_string()}")
+
+
+session = zenoh.open(config)
+session_info = session.info
+print(f"Session info:\n  - Routers zids:{[zid for zid in session_info.routers_zid()]}")
+print("Declaring subscriber for chatter")
+subscriber = session.declare_subscriber("chatter", sample_callback)
