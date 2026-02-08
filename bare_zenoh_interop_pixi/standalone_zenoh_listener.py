24f61c0b6b6c1f45da031974505ad1559f240649
Dan Zimmerman <code@danzimmerman.com>
2026-02-08 15:39:59 -0500

    Delete unused code

--- bare_zenoh_interop_pixi/standalone_zenoh_listener.py
+++ bare_zenoh_interop_pixi/standalone_zenoh_listener.py
@@ -4,14 +4,6 @@ import zenoh
 import rosbags
 from cdr_decoder import CDRStructBase
 
-
-class StdMsgsString(CDRStructBase):
-    data: str
-
-
-def chatter_callback(sample):
-    print(StdMsgsString.deserialize(sample.payload))
-
 config = zenoh.Config.from_file("config.json5")
 with zenoh.open(config) as session:
     with session.declare_subscriber("demo/example/**") as subscriber:
